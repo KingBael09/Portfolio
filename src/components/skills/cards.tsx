8bb23756bae902aae0962b5e96695f20e0f0c71d
KingBael <www.jayesh567@gmail.com>
2023-01-03 17:07:17 +0530

    Fixed Transition

--- src/components/skills/cards.tsx
+++ src/components/skills/cards.tsx
@@ -1,41 +1,60 @@
 import ButtonBase from "@mui/material/ButtonBase";
 import { typedata, typenode } from "../../data/skillsData";
 import styles from "../../styles/Skills.module.css";
+import { useRef } from "react";
+import { CSSTransition } from "react-transition-group";
+
 interface cardType {
   mapData: typedata;
+  action: any;
+  delay: number;
 }
 
 const viewportWidth = window.innerWidth;
 
 export const SkillCards = (props: cardType) => {
   const e = props.mapData;
-  return (
-    <div className={styles.card}>
-      <ButtonBase
-        className={styles.touch}
-        disableRipple={viewportWidth >= 850 ? false : true}
-        color="inherit"
-        component={"div"}
-      >
-        <div className={styles.wrapper}>
-          {e.intNode.length
-            ? e.intNode.map((elm: typenode) => {
-                return (
-                  <div key={elm.name} className={styles.addInfo}>
-                    <div className={styles.altContainer}>
-                      <span className={styles.altImage}>{elm.icon}</span>
-                      <span className={styles.addName}>{elm.name}</span>
-                    </div>
-                  </div>
-                );
-              })
-            : null}
-          <div className={styles.icon}> {e.icon}</div>
-          <div className={styles.name}>{e.name}</div>
+  const loadComponent = props.action;
+  const nodeRef = useRef(null);
+  const delay = props.delay;
 
-          <div className={styles.level}>{e.level}</div>
-        </div>
-      </ButtonBase>
-    </div>
+  return (
+    <CSSTransition
+      in={loadComponent}
+      nodeRef={nodeRef}
+      timeout={100 + delay}
+      classNames={{
+        enterActive: styles.cardEnter,
+        enterDone: styles.cardEnterActive,
+      }}
+    >
+      <div ref={nodeRef} className={styles.card}>
+        <ButtonBase
+          className={styles.touch}
+          disableRipple={viewportWidth >= 850 ? false : true}
+          color="inherit"
+          component={"div"}
+        >
+          <div className={styles.wrapper}>
+            {e.intNode.length
+              ? e.intNode.map((elm: typenode) => {
+                  return (
+                    <div key={elm.name} className={styles.addInfo}>
+                      <div className={styles.altContainer}>
+                        <span className={styles.altImage}>{elm.icon}</span>
+                        <span className={styles.addName}>{elm.name}</span>
+                      </div>
+                    </div>
+                  );
+                })
+              : null}
+            <div className={styles.icon}> {e.icon}</div>
+            <div className={styles.name}>{e.name}</div>
+
+            <div className={styles.level}>{e.level}</div>
+          </div>
+        </ButtonBase>
+      </div>
+    </CSSTransition>
   );
 };
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -53,29 +53,24 @@ const About = () => {
             #About Me
           </span>
         </div>
-        <div className={styles.section}>
-          <div className={styles.about}>
-            <div className={styles.highlight}>
-              Hello I'm <span className={styles.name}>Jayesh</span>
-            </div>
-            <div className={styles.content}>
-              <span>
-                I am an aspiring Full-Stack Developer from India Looking for
-                Opportunities.
-              </span>
-              <span> I like Coding, Watching Anime & Playing Games</span>
-              <br />
-              <span>
-                TypeScript is The Best....{" "}
-                <span className={styles.subMsg}>
-                  The Autocomplete is Godsent
-                </span>
-              </span>
-              <br />
-              <span>My Favourite Framework : NextJS</span>
-            </div>
+        <div className={styles.about}>
+          <div className={styles.highlight}>
+            Hello I'm <span className={styles.name}>Jayesh</span>
           </div>
-          <div className={styles.interest}>Interest</div>
+          <div className={styles.content}>
+            <span>
+              I am an aspiring Full-Stack Developer from India Looking for
+              Opportunities.
+            </span>
+            <span> I like Coding, Watching Anime & Playing Games</span>
+            <br />
+            <span>
+              TypeScript is The Best....{" "}
+              <span className={styles.subMsg}>The Autocomplete is Godsent</span>
+            </span>
+            <br />
+            <span>My Favourite Framework : NextJS</span>
+          </div>
         </div>
       </div>
     </div>
