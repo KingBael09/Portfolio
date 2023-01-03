fc7badd553e61585a4473efa691494e7d35001ca
KingBael <www.jayesh567@gmail.com>
2023-01-03 10:00:53 +0530

    Stable release

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -9,9 +9,9 @@ import { useProjectContext } from "../context/sessionProj";
 import { DocumentTitle } from "../hooks/docTitle";
 import Snackbar from "@mui/material/Snackbar";
 
-// const url: string = "https://api.github.com/users/KingBael09/repos";
+const url: string = "https://api.github.com/users/KingBael09/repos";
 // const url: string = "https://animechan.vercel.app/api/random";
-const url: string = "lmao";
+// const url: string = "lmao";
 
 const Projects = () => {
   DocumentTitle("Projects");
